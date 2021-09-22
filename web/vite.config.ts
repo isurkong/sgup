f66f3d4bc658023f24c9564946a69bcc9f07c4ed
isurkong <wulilin_email@163.com>
2021-09-22 19:52:57 +0800

    add element-plus config for vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    ElementPlus(),
+  ]
 })
