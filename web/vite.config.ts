1b582c19e49f891061881aef046497b0ccd64ac1
isurkong <wulilin_email@163.com>
2021-09-29 00:00:00 +0800

    add vue-route@4

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,14 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import ElementPlus from 'unplugin-element-plus/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({mode}) => defineConfig({
   plugins: [
     vue(),
     Components({
       resolvers: [ElementPlusResolver()],
     }),
     ElementPlus(),
-  ]
+  ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname,'./'),
+      '@': path.resolve(__dirname,'src/')
+    },
+  }
 })
