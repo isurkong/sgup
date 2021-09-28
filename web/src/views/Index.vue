834ee9e0e3a3a0d31e3f3920246b48bc0918af41
isurkong <wulilin_email@163.com>
2021-09-29 00:03:34 +0800

    "add config for vue-route@4"

---
+++ web/src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{props.msg}}</h1>
+    <button>click me</button>
+</template>
+
+
+<script setup lang="ts">
+    const ENV = import.meta.env
+    console.log('env:',ENV)
+    const props = defineProps({
+        msg: {
+            type: String,
+            default: ()=>'我是home组件',
+        }
+    })
+</script>
